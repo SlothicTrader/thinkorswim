68e817189c87c533e2c37dac77e2b96a47bef19a
hummingSloth <84049042+hummingSloth@users.noreply.github.com>
2022-03-05 12:24:37 -0700

    Delete hs-vwap-column.ts

--- hs-vwap-column.ts
+++
@@ -1,4 +0,0 @@
-plot vwap = vwap();
-AddLabel(yes, "vwap", Color.CurRENT);
-
-AssignBackgroundColor(if close > vwap then Color.DARK_GREEN else if close < vwap then Color.DARK_RED else Color.DaRK_GREEN);
