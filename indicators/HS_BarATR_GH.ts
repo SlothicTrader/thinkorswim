0edf349cfcb6d39cf3b85157aa1565bdad4b34db
hummingSloth <84049042+hummingSloth@users.noreply.github.com>
2022-03-05 12:36:44 -0700

    Create HS_BarATR_GH.ts

---
+++ indicators/HS_BarATR_GH.ts
@@ -0,0 +1,8 @@
+#Study name HS_BarATR_GH
+#by hummingSloth https://github.com/hummingSloth/thinkorswim
+
+input ATRLength = 14;
+def ATR = Round(Average(TrueRange(high,  close,  low),  ATRLength), 2);
+def iv = Round(close() * (imp_volatility() / 15.87), 3);
+AddLabel(yes, Concat("ATR=", ATR), Color.YELLOW);
+AddLabel(yes, Concat("ExpM=", iv), Color.YELLOW);
