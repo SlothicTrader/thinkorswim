0e2106f015e1ac5c0b28e844ec63c4a7256b4e82
hummingSloth <84049042+hummingSloth@users.noreply.github.com>
2022-03-05 12:24:28 -0700

    Delete hs-powerPanel.ts

--- hs-powerPanel.ts
+++
@@ -1,5 +0,0 @@
-declar upper;
-
-input showPositivePriceAction = yes;
-
-AddLabel(showPositivePriceAction, "vwap", (if close() > vwap(period = AggregationPeriod.DAY) then Color.GREEN else Color.RED));
