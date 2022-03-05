96365b37fbb4c86e288f112eb7e7a12b1b8c303f
hummingSloth <84049042+hummingSloth@users.noreply.github.com>
2022-03-05 12:24:43 -0700

    Delete hs_PDRLabel.ts

--- hs_PDRLabel.ts
+++
@@ -1,9 +0,0 @@
-declare upper;
-
-def Period1Day = AggregationPeriod.DAY;
-
-def isOutsidePDR = if (close(period = Period1Day) < low(period = Period1Day)[1]) or (close(period = Period1Day) > high(period = Period1Day)[1]) then yes else no;
-def isInsidePDR = if (close(period = Period1Day) < high(period = Period1Day)[1]) and (close(period = Period1Day) > low(period = Period1Day)[1]) then yes else no;
-
-AddLabel(isOutsidePDR, "Outside PDR", Color.UPTICK);
-AddLabel(isInsidePDR, "Inside PDR", Color.ORANGE);
