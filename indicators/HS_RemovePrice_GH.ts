fe7630467798902b065cf84b15ce0a7096c8809f
hummingSloth <84049042+hummingSloth@users.noreply.github.com>
2022-03-05 13:18:54 -0700

    Create HS_RemovePrice_GH.ts

---
+++ indicators/HS_RemovePrice_GH.ts
@@ -0,0 +1,8 @@
+# Study name HS_HiLows_GH
+# by hummingSloth https://github.com/hummingSloth/thinkorswim
+# version 1.0
+
+declare upper;
+
+input showPricePlot = no;
+hidePricePlot(!showPricePlot);
